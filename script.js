59bac353bc5291bade3efbcaa7a75de7e1fed5db
pranto <prantor278@gmail.com>
2025-08-29 13:30:31 +0600

    added main section funtionality with call history

--- script.js
+++ script.js
@@ -18,3 +18,103 @@ document.getElementById("heart-icon-antiCourrption").addEventListener("click",ge
 document.getElementById("heart-icon-electricity").addEventListener("click",getTheHeartCount)
 document.getElementById("heart-icon-brac").addEventListener("click",getTheHeartCount)
 document.getElementById("heart-icon-railway").addEventListener("click",getTheHeartCount)
+
+//for main section funtionality
+
+function callMake(serviceNameId,serviceNumberId){
+    const serviceName=document.getElementById(serviceNameId).innerText
+    const serviceNumber=document.getElementById(serviceNumberId).innerText
+
+    alert(serviceName + " " + serviceNumber);
+
+    const coin=document.getElementById("coin").innerText;
+    const coinNumber=parseInt(coin);
+
+    if(coinNumber<20){
+        alert("you dont have enough coin");
+        return;
+    }
+
+    const newcoin=coinNumber-20;
+    document.getElementById("coin").innerText=newcoin;
+
+
+    const now=new Date();
+    const time=now.toLocaleTimeString("en-US",{
+        hour12:false,
+        hour:"2-digit",
+        minute:"2-digit",
+        second:"2-digit"
+
+    })
+
+    const historyList=document.getElementById("history-list");
+    historyList.innerHTML+=`
+        <div class="flex justify-between items-center p-5 mb-2 rounded-lg bg-gray-200 mx-6">
+            <div>
+                <p class="font-semibold">${serviceName}</p>
+                <p class="text-sm font-semibold text-gray-600">${serviceNumber}</p>
+            </div>
+            <div class="text-xl text-gray-700">${time}</div>
+        </div>
+    `;
+    
+    const callHistoryBox = document.querySelector(".call-history");
+    callHistoryBox.classList.remove("bg-white");
+    callHistoryBox.classList.add("bg-gray-100");
+}
+
+// apply for every cart
+//for national emergency
+document.getElementById("call-btn-national").addEventListener("click",function(){
+    callMake("service-name-national","service-number-national")
+})
+// police help line
+document.getElementById("call-btn-police").addEventListener("click",function(){
+    callMake("service-name-police","service-number-police")
+})
+//fire service
+document.getElementById("call-btn-fire").addEventListener("click",function(){
+    callMake("service-name-fire","service-number-fire")
+})
+
+//ambulance
+document.getElementById("call-btn-ambulance").addEventListener("click",function(){
+    callMake("service-name-ambulance","service-number-ambulance")
+})
+
+//womenChild
+
+document.getElementById("call-btn-womenChild").addEventListener("click",function(){
+    callMake("service-name-womenChild","service-number-womenChild")
+})
+
+//anti corruption
+
+document.getElementById("call-btn-anti").addEventListener("click",function(){
+    callMake("service-name-anti","service-number-anti")
+})
+
+//electricity
+
+document.getElementById("call-btn-electricity").addEventListener("click",function(){
+    callMake("service-name-electricity","service-number-electricity")
+})
+
+//brac
+
+document.getElementById("call-btn-brac").addEventListener("click",function(){
+    callMake("service-name-brac","service-number-brac")
+})
+
+//railway
+document.getElementById("call-btn-railway").addEventListener("click",function(){
+    callMake("service-name-railway","service-number-railway")
+})
+
+
+document.getElementById("clear-btn").addEventListener("click",function(){
+    const historydlt=document.getElementById("history-list");
+    historydlt.innerHTML="";
+})
+
